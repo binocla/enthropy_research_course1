de97cd261b7ad624cc226170795307d97706be77
Sergey Shamov <sergey.shamov@mera.com>
2021-12-11 19:33:36 +0300

    StringBuilder
    Took 44 minutes

---
+++ src/space/enthropy/lesson10/Main.java
@@ -0,0 +1,11 @@
+package space.enthropy.lesson10;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String b = "Binocla";
+        for (int i = 0; i < 2; i++) {
+            s += b + " ";
+        }
+    }
+}
