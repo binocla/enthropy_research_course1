c6071bfa7feda2e8cfa6794ba6de0ce9afde4def
Sergey Shamov <sergey.shamov@mera.com>
2021-12-18 21:03:28 +0300

    Exceptions curious example
    Took 4 minutes

--- src/space/enthropy/lesson10/Main.java
+++ src/space/enthropy/lesson10/Main.java
@@ -2,10 +2,17 @@ package space.enthropy.lesson10;
 
 public class Main {
     public static void main(String[] args) {
-        String s = "Hello";
-        String b = "Binocla";
-        for (int i = 0; i < 2; i++) {
-            s += b + " ";
+        System.out.println(res());
+    }
+
+    public static int res() {
+        try {
+            System.out.println(1 / 0);
+            return 2;
+        } catch (ArithmeticException e) {
+            return 5;
+        } finally {
+            return 10;
         }
     }
 }
